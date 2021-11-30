0eb067b7148a19f01abfd0107c305dd6eae308eb
dbruski <dawid.bruski1@gmail.com>
2021-11-30 21:36:07 +0100

    Added handling error state on fetching videos page

--- client/src/ducks/modules/SearchVideos/searchVideosSlice.ts
+++ client/src/ducks/modules/SearchVideos/searchVideosSlice.ts
@@ -6,6 +6,7 @@ import { SearchVideosStateI } from './searchVideosSlice.types';
 const initialState: SearchVideosStateI = {
   videos: [],
   isLoading: false,
+  status: 'NOT_SET',
 };
 
 export const getVideosByPhrase = createAsyncThunk('searchVideos', async (phrase: string) => {
@@ -15,20 +16,29 @@ export const getVideosByPhrase = createAsyncThunk('searchVideos', async (phrase:
 const searchVideosSlice = createSlice({
   name: 'searchVideos',
   initialState,
-  reducers: {},
+  reducers: {
+    clearVideos: state => {
+      state.status = 'NOT_SET';
+      state.videos = [];
+    }
+  },
   extraReducers: {
     [getVideosByPhrase.pending.toString()]: state => {
       state.isLoading = true;
     },
     [getVideosByPhrase.rejected.toString()]: (state) => {
       state.isLoading = false;
+      state.status = 'ERROR';
       toast.error('Nie udało się pobrać danych z YouTube API');
     },
     [getVideosByPhrase.fulfilled.toString()]: (state, action) => {
       state.isLoading = false;
+      state.status = 'SUCCESS';
       state.videos = action.payload.data.items;
     },
   },
 });
+
+export const { clearVideos } = searchVideosSlice.actions;
 
 export default searchVideosSlice.reducer;
