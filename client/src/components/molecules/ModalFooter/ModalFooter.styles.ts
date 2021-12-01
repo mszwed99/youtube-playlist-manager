9cd96fac2004bb1473634a829d06d1c9c8088dea
dbruski <dawid.bruski1@gmail.com>
2021-12-01 22:19:26 +0100

    Added ModalFooter

---
+++ client/src/components/molecules/ModalFooter/ModalFooter.styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const FooterContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: center;
+  justify-content: space-around;
+`;
