55cd55d560bc046c432d89a4bbe62309b11aeec3
dbruski <dawid.bruski1@gmail.com>
2021-12-14 21:50:50 +0100

    Update VideoCardLoading.component.tsx

--- client/src/components/organisms/VideoCardLoading/VideoCardLoading.component.tsx
+++ client/src/components/organisms/VideoCardLoading/VideoCardLoading.component.tsx
@@ -12,13 +12,13 @@ export const VideoCardLoading: React.FC = () => {
       <ContentLoader
         speed={1}
         width={width}
-        height={240}
-        viewBox={`0 0 ${width} 240`}
+        height={200}
+        viewBox={`0 0 ${width} 200`}
         backgroundColor="#fff"
         foregroundColor="#777"
-        style={{ width: '60vw', height: 240 }}
+        style={{ width: '60vw', height: 200 }}
       >
-        <rect x="0" y={spacing.small} rx="3" ry="3" width="360" height={240 - spacing.medium} />
+        <rect x="0" y={spacing.small} rx="3" ry="3" width="360" height={200 - spacing.medium} />
         <rect x={360 + spacing.large} y={0 + spacing.small} rx="3" ry="3" width="40%" height={spacing.large} />
         <rect x={360 + spacing.large} y="30%" rx="3" ry="3" width="15%" height="16" />
         <rect x={360 + spacing.large} y="50%" rx="3" ry="3" width="15%" height="6" />
