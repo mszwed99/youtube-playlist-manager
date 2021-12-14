dd522c8a81bfff8cc6171bffbb4313383513a938
dbruski <dawid.bruski1@gmail.com>
2021-12-14 21:50:45 +0100

    Edited videoCard compnent

--- client/src/components/organisms/VideoCard/VideoCard.component.tsx
+++ client/src/components/organisms/VideoCard/VideoCard.component.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
 import moment from 'moment';
 import { useHistory } from 'react-router';
-import { Description, PublishTime, Title, Channel, VideoCardContainer, VideoDetails, VideoThumbnail } from './VideoCard.styles';
+import { FiFolderPlus } from "react-icons/fi";
+import { mapVideoSnippetToVideoModel } from 'ducks/modules/SearchVideos/searchVideosSlice';
+import { Description, PublishTime, Title, Channel, VideoCardContainer, VideoDetails, VideoThumbnail, Plus } from './VideoCard.styles';
 import { VideoCardPropsI } from './VideoCard.types';
 
-export const VideoCard: React.FC<VideoCardPropsI> = ({ video }) => {
+export const VideoCard: React.FC<VideoCardPropsI> = ({ video, onPressAddVideo }) => {
   const history = useHistory();
   const formatPublishDate = (timestamp: string): string => moment(timestamp).format("DD MM YYYY");
 
   const onPressVideo = () => {
-    history.push(`/video/${video.id.videoId}`);
+    history.push(`/video/${video.id.videoId}`, { video: mapVideoSnippetToVideoModel(video) });
+  }
+
+  const onPressAddToPlaylist = (e: any) => {
+    e.stopPropagation();
+    onPressAddVideo(mapVideoSnippetToVideoModel(video));
   }
 
   return (
-    <VideoCardContainer onClick={onPressVideo}>
-      <VideoThumbnail src={video.snippet.thumbnails.medium.url} />
-      <VideoDetails>
-        <Title>{video.snippet.title}</Title>
-        <Channel>{video.snippet.channelTitle}</Channel>
-        <PublishTime>Dodano: {formatPublishDate(video.snippet.publishedAt)}</PublishTime>
-        <Description>{video.snippet.description}</Description>
-      </VideoDetails>
-    </VideoCardContainer>
+    <>
+      <VideoCardContainer onClick={onPressVideo}>
+        <Plus onClick={onPressAddToPlaylist}><FiFolderPlus /></Plus>
+        <VideoThumbnail src={video.snippet.thumbnails.medium.url} />
+        <VideoDetails>
+          <Title>{video.snippet.title}</Title>
+          <Channel>{video.snippet.channelTitle}</Channel>
+          <PublishTime>Dodano: {formatPublishDate(video.snippet.publishedAt)}</PublishTime>
+          <Description>{video.snippet.description}</Description>
+        </VideoDetails>
+      </VideoCardContainer>
+    </>
   );
 };
